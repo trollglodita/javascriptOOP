50b4cf416821f6910b3317d9476f2e84cdb2d713
trollglodita <1trollglodita@gmail.com>
2013-12-07 18:54:50 -0300

    DRY and Walls interface

--- js/maze.js
+++ js/maze.js
@@ -10,32 +10,49 @@ function Maze (width, height) {
 	this.endX 		      = null;
 	this.endY 			  = null;
 
+	this.directions = ["north", "east", "south", "west"];
 	this.spaces = [];
 
 	var x, y;
 	for (x=1; x <= width; x += 1) {
 		this.spaces[x] = [];
 		for (y=1; y <= height; y += 1){
-			this.spaces[x][y] = new MazeSpace();
+			this.spaces[x][y] = new MazeSpace(this.directions);
 		}
 	}
 }
 
 Maze.prototype.setStart = function(x, y, orientation) {
-	this.startX = x;
-	this.startY = y;
-	this.startOrientation = orientation;
+	if (this.isInBounds(x, y) && this.isValidDirection(orientation)) {
+		this.startX = x;
+		this.startY = y;
+		this.startOrientation = orientation;
+		return true;
+	}
+	return false;
 };
 
 Maze.prototype.setEnd = function(x, y) {
+	if (!this.isInBounds(x, y)) {
+		return false;
+	}
 	this.endX = x;
 	this.endY = y;
+	return true;
 };
 
 Maze.prototype.setWall = function(x, y, direction) {
-	if (x > 0 && x <= this.width && y > 0 && y <= this.height && ["north","east","south","west"].indexOf(direction) != -1) {
+	if (this.isInBounds && this.isValidDirection) {
 		this.spaces[x][y].setWall(direction);
 		return true;
 	}
 	return false;
+};
+
+Maze.prototype.isValidDirection = function(direction) {
+	return this.directions.indexOf(direction) != -1;
+};
+
+Maze.prototype.isInBounds = function(x, y) {
+	return x > 0 && x <= this.width && y > 0 && y <= this.height;
 };
