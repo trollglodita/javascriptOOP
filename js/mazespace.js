239afc694e9b4689c6e7b27e320b0273a0e61e1b
trollglodita <1trollglodita@gmail.com>
2013-12-07 17:32:23 -0300

    MazeSpaces and Walls prototype

---
+++ js/mazespace.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function MazeSpace () {
+	this.north = false;
+	this.east = false;
+	this.south = false;
+	this.west = false;
+}
+
+MazeSpace.prototype.setWall = function(direction) {
+	this["direction"] = true;
+};
