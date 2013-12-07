81a28faab4f728962736df8b9011a71a8b06c6c5
trollglodita <1trollglodita@gmail.com>
2013-12-07 16:53:08 -0300

    Start position

--- js/maze.js
+++ js/maze.js
@@ -3,4 +3,21 @@
 function Maze (width, height) {
 	this.width = width;
 	this.height = height;
+
+	this.startX 		  = null;
+	this.startY 		  = null;
+	this.startOrientation = null;
+	this.endX 		      = null;
+	this.endY 			  = null;
 }
+
+Maze.prototype.setStart = function(x, y, orientation) {
+	this.startX = x;
+	this.startY = y;
+	this.startOrientation = orientation;
+};
+
+Maze.prototype.setEnd = function(x, y) {
+	this.endX = x;
+	this.endY = y;
+};
